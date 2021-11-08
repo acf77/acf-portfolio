32fd0ed0bc8e7fb24ae615f180d50fd589577a2e
acf77 <antoniocarlos.j77@gmail.com>
2021-11-08 17:42:41 -0300

    responsive ready, adding gh and lin icons

--- acfilho.dev/src/App.js
+++ acfilho.dev/src/App.js
@@ -1,24 +1,34 @@
 import React from "react";
 
 import "./App.css";
+
 import heroImage from "./assets/hero-mockup.png";
+import Github from './assets/icon-github.svg';
+import Linkedin from './assets/icon-linkedin.svg'
 
 function App() {
   return (
     <body>
-      <div class="flex-hero-container">
-        <div class="flex-left">
-          <h2>Hi there, I'm Antonio Carlos</h2>
-          <h1>
-            Coding your business
-            <br />
-            whatever the screen size
-          </h1>
+      <div class="wrapper">
+        <div class="flex-hero-container">
+          <div class="flex-left">
+            <h2>Hi there, I'm Antonio Carlos</h2>
+            <h1>
+              Coding your business
+              <br />
+              whatever the screen size
+            </h1>
 
-          <button class="button">Get in touch</button>
-        </div>
-        <div class="flex-right">
-          <img src={heroImage} class="heroImage" />
+            <div class='hero-button-icons-flex'>
+              <button class="button">Get in touch</button>
+              <img src={Github} class='icon-github' />
+              <img src={Linkedin} class='icon-linkedin' />
+
+            </div>
+          </div>
+          <div class="flex-right">
+            <img src={heroImage} class="heroImage" />
+          </div>
         </div>
       </div>
     </body>
