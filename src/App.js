d588d1ed1e45830f417d91288054e3641d51bf99
acf77 <antoniocarlos.j77@gmail.com>
2021-11-09 18:03:01 -0300

    deleted subfolder

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ import Github from "./assets/icon-github.svg";
 import Linkedin from "./assets/icon-linkedin.svg";
 
 function App() {
+  function githubLink() {
+    const link = "https://github.com/acf77";
+    window.open(link);
+  }
+
+  function linkedinLink() {
+    const link = "https://www.linkedin.com/in/acfilho/";
+    window.open(link);
+  }
+
   return (
     <body>
       <div class="wrapper">
@@ -21,8 +31,16 @@ function App() {
 
             <div class="hero-button-icons-flex">
               <button class="button">Get in touch</button>
-              <img src={Github} class="icon-github" />
-              <img src={Linkedin} class="icon-linkedin" />
+              <img
+                src={Github}
+                class="icon-github"
+                onClick={() => githubLink()}
+              />
+              <img
+                src={Linkedin}
+                class="icon-linkedin"
+                onClick={() => linkedinLink()}
+              />
             </div>
           </div>
           <div class="flex-right">
