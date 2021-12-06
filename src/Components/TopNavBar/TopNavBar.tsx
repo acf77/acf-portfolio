6d3d07fbfdfd5b3260e4214cb600508837a60c09
acf77 <antoniocarlos.j77@gmail.com>
2021-12-06 12:01:45 -0300

    added overly to slide card - deploy ready

--- src/Components/TopNavBar/TopNavBar.tsx
+++ src/Components/TopNavBar/TopNavBar.tsx
@@ -1,7 +1,4 @@
 import React, { useEffect, useState } from "react";
-//@ts-ignore
-import { Link, animateScroll as scroll } from "react-scroll";
-
 import "./TopNavBar.css";
 
 interface TopNavBarProps {
